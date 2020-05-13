14fb524127e46bb08b4c9f3d1c7d51f8c7bfb3bf
Waldecleber <waldecleber@gmail.com>
2020-05-13 15:01:43 -0300

    Fix project

--- api-valida-cpf/src/main/java/br/com/sicredi/api/validacpf/enums/StatusVote.java
+++
@@ -1,7 +0,0 @@
-package br.com.sicredi.api.validacpf.enums;
-
-public enum StatusVote {
-
-	ABLE_TO_VOTE,
-	UNABLE_TO_VOTE
-}
--- api-valida-cpf/src/main/java/br/com/sicredi/api/validacpf/service/ValidadorService.java
+++
@@ -1,8 +0,0 @@
-package br.com.sicredi.api.validacpf.service;
-
-import br.com.sicredi.api.validacpf.enums.StatusVote;
-
-public interface ValidadorService {
-
-	StatusVote validaCpf(String cpf);
-}
